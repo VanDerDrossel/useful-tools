cbbf3959265801d1c05dadd1eea2b79efc54b4df
Anton N <71944284+VanDerDrossel@users.noreply.github.com>
2023-09-14 15:01:37 +0300

    Update tools.py
    add adjust_control_sum()

--- tools.py
+++ tools.py
@@ -30,3 +30,20 @@ def group_rows_from_file(source_file: str,
     with open(result_file, 'w') as file:
         for row in create_partition(part_size, read_lst_fromsource_file):
             print(*row, sep=separator, file=file)
+
+
+def adjust_control_sum(inn: str) -> str:
+    """Function for change control sum, if INN is not correct. 
+    If INN is correct, then return result will be equal passed argument.
+    """
+        
+    # For FL 12 char
+    if len(inn) == 12 and isinstance(int(inn), int):
+        base = [int(i) for i in inn[0:10]]
+        
+        control_number_11 = ((7*base[0] + 2*base[1] + 4*base[2] + 10*base[3] + 3*base[4] + 5*base[5] + 9*base[6] + 4*base[7] + 6*base[8] + 8*base[9]) % 11) % 10
+        base.append(control_number_11)
+        control_number_12 = ((3*base[0] +  7*base[1] + 2*base[2] + 4*base[3] + 10*base[4] + 3*base[5] + 5*base[6] +  9*base[7] + 4*base[8] + 6*base[9] +  8*base[10]) % 11) % 10
+        base.append(control_number_12)
+        
+        return ''.join(map(str, base))
